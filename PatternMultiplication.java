f62bcd6949307af379bc2bb7f51b5c796361cddd
Payal Singla <singlapayal43@gmail.com>
2020-08-18 22:53:37 +0530

    Update PatternMultiplication.java

--- PatternMultiplication.java
+++ PatternMultiplication.java
@@ -1,1 +1,3 @@
-
+"Implement a Java-main-method that prints out the multiplication table for all numbers from 1 to 10.
+"Use the tabulator character \t' to align the values. A sample output is shown below.
+  
