4b08c6a441ccea57002cb69922f55e1223fa0529
Payal Singla <singlapayal43@gmail.com>
2020-08-18 22:54:45 +0530

    Update PatternMultiplication.java

--- PatternMultiplication.java
+++ PatternMultiplication.java
@@ -1,3 +1,21 @@
 "Implement a Java-main-method that prints out the multiplication table for all numbers from 1 to 10.
 "Use the tabulator character \t' to align the values. A sample output is shown below.
   
+  public class Multi {
+   public static void main(String[] args) {
+       int size = 11;
+      int row=1;
+      while(row<size){  
+         System.out.printf("%2d ", row); 
+          
+          int column=2;
+         while(column <size){  
+            System.out.printf("%4d", row * column);
+            column++;
+         }
+         row++;
+         System.out.println(); 
+         
+      }
+   }
+}
