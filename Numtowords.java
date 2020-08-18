3b34a91b2c778b6a381653c07f72ade01a47406d
Payal Singla <singlapayal43@gmail.com>
2020-08-18 22:44:42 +0530

    Update Numtowords.java

--- Numtowords.java
+++ Numtowords.java
@@ -1,1 +1,345 @@
+public class Convo {
+
+	 public static void main(String[] args) {
+
+		long i = 0;
+
+		switch(args[0]) {
+
+			case "one":
+
+				i=1;
+
+				break;
+
+			case "two":
+
+				i=2;
+
+				break;
+
+			case "three":
+
+				i=3;
+
+				break;
+
+			case "four":
+
+				i=4;
+
+				break;
+
+			case "five":
+
+				i=5;
+
+				break;
+
+			case "six":
+
+				i=6;
+
+				break;
+
+			case "seven":
+
+				i=7;
+
+				break;
+			case "eight":
+				i=8;
+				break;
+			case "nine":
+				i=9;
+				break;
+			case "ten":
+				i=10;
+				break;
+			case "eleven":
+				i=11;
+				break;
+			case "twelve":
+				i=12;
+				break;
+			case "thirteen":
+				i=13;
+				break;
+			case "fourteen":
+				i=14;
+				break;
+			case "fifteen":
+				i=15;
+				break;
+			case "sixteen":
+				i=16;
+				break;
+			case "seventeen":
+				i=17;
+				break;
+			case "eighteen":
+				i=18;
+				break;
+			case "nineteen":
+				i=19;
+				break;
+			case "twenty":
+				i=20;
+				break;
+			case "twenty one":
+				i=21;
+				break;
+			case "twenty two":
+				i=22;
+				break;
+			case "twenty three":
+				i=23;
+				break;
+			case "twenty four":
+				i=24;
+				break;
+			case "twenty five":
+				i=25;
+				break;
+			case "twenty six":
+				i=26;
+				break;
+			case "twenty seven":
+				i=27;
+				break;
+			case "twenty eight":
+				i=28;
+				break;
+			case "twenty nine":
+				i=29;
+				break;
+			case "thirty":
+
+				i=30;
+				break;
+			case "thirty one":
+				i=31;
+				break;
+			case "thirty two":
+				i=32;
+				break;
+			case "thirty three":
+				i=33;
+				break;
+			case "thirty four":
+				i=34;
+				break;
+			case "thirty five":
+				i=35;
+				break;
+			case "thirty six":
+				i=36;
+				break;
+			case "thirty seven":
+				i=37;
+				break;
+			case "thirty eight":
+				i=38;
+				break;
+			case "thirty nine":
+				i=39;
+				break;
+			case "fourty":
+				i=40;
+				break;
+			case "fourty one":
+				i=41;
+				break;
+			case "fourty two":
+				i=42;
+				break;
+			case "fourty three":
+				i=43;
+				break;
+			case "fourty four":
+				i=44;
+				break;
+			case "fourty five":
+				i=45;
+				break;
+			case "fourty six":
+				i=46;
+				break;
+			case "fourty seven":
+				i=47;
+				break;
+			case "fourty eight":
+				i=48;
+				break;
+			case "fourty nine":
+				i=49;
+				break;
+			case "fifty":
+				i=50;
+				break;
+			case "fifty one":
+				i=51;
+				break;
+			case "fifty two":
+				i=52;
+				break;
+			case "fifty three":
+				i=53;
+				break;
+			case "fifty four":
+				i=54;
+
+				break;
+
+			case "fifty five":
+
+				i=55;
+
+				break;
+
+			case "fifty six":
+
+				i=56;
+
+				break;
+			case "fifty seven":
+				i=57;
+				break;
+			case "fifty eight":
+				i=58;
+				break;
+			case "fifty nine":
+				i=59;
+				break;
+			case "sixty":
+				i=60;
+				break;
+			case "sixty one":
+				i=61;
+				break;
+			case "sixty two":
+				i=62;
+				break;
+			case "sixty three":
+				i=63;
+				break;
+			case "sixty four":
+				i=64;
+				break;
+			case "sixty five":
+				i=65;
+				break;
+			case "sixty six":
+				i=66;
+				break;
+			case "sixty seven":
+				i=67;
+				break;
+			case "sixty eight":
+				i=68;
+				break;
+			case "sixty nine":
+				i=69;
+				break;
+			case "seventy":
+				i=70;
+				break;
+			case "seventy one":
+				i=71;
+				break;
+			case "seventy two":
+				i=72;
+				break;
+			case "seventy three":
+				i=73;
+				break;
+			case "seventy four":
+				i=74;
+				break;
+			case "seventy five":
+				i=75;
+				break;
+			case "seventy six":
+				i=76;
+				break;
+			case "seventy seven":
+				i=77;
+				break;
+			case "seventy eight":
+				i=78;
+				break;
+			case "seventy nine":
+				i=79;
+				break;
+			case "eighty":
+				i=80;
+				break;
+			case "eighty one":
+				i=81;
+				break;
+			case "eighty two":
+				i=82;
+				break;
+			case "eighty three":
+				i=83;
+				break;
+			case "eighty four":
+				i=84;
+				break;
+			case "eighty five":
+				i=85;
+				break;
+			case "eighty six":
+				i=86;
+				break;
+			case "eighty seven":
+				i=87;
+				break;
+			case "eighty eight":
+				i=88;
+				break;
+			case "eighty nine":
+				i=89;
+				break;
+			case "ninety":
+				i=90;
+				break;
+			case "ninety one":
+				i=91;
+				break;
+			case "ninety two":
+				i=92;
+				break;
+			case "ninety three":
+				i=93;
+				break;
+			case "ninety four":
+				i=94;
+				break;
+			case "ninety five":
+				i=95;
+				break;
+			case "ninety six":
+				i=96;
+				break;
+			case "ninety seven":
+				i=97;
+				break;
+			case "ninety eight":
+				i=98;
+				break;
+			case "ninety nine":
+				i=99;
+				break;
+			case "one hundred":
+				i=100;
+				break;
+			default:
+				break;
+
+		}
+		System.out.println("Entered number is " + i);
+
+		}
+}
 
