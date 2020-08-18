5447dc0147ddd9b0f7ba6a54f435771782f2a514
Payal Singla <singlapayal43@gmail.com>
2020-08-18 22:52:16 +0530

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,1 +1,35 @@
+import java.util.Scanner;
+
+public class Table{
+
+	static void table() 
+{
+		Scanner n = new Scanner(System.in);
+
+		int num,i,x;
+
+		System.out.println("enter the number");
+
+		num = n.nextInt();
+
+		for(i=1;i<=10;i++)
+
+		{
+
+			x=num*i;
+
+			System.out.println(num+"*"+i+"="+x);
+
+		}
+	
+}
+
+	public static void main(String[] args) {
+
+			table();
+	
+}
+	
+
+}
 
