e46abf17e43e87d832bc99a50d180b2981b8a21a
Payal Singla <singlapayal43@gmail.com>
2020-08-18 22:47:01 +0530

    Update Loops.java

--- Loops.java
+++ Loops.java
@@ -1,1 +1,41 @@
+public class Loop {
+	public static void main(String[] args) {
+
+		System.out.println("For loop:");
+
+		for(int x=1;x<=10;x++){
+	
+		System.out.println(x);
+
+		}
+
+		System.out.println("While loop:");
+
+		int y=1;
+
+		while(y<=10){
+
+			System.out.println(y);
+
+			y++;
+
+		}
+
+		System.out.println("do-while loop:");
+
+		int z=1;
+
+		do {
+
+			System.out.println(z);
+
+			z++;
+
+		}
+
+		while(z<=10);
+
+	}
+
+}
 
